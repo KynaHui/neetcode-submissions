0d7b433aeb147686f5b87505816bae9b32279c28
KynaHui <141233615+KynaHui@users.noreply.github.com>
2026-07-17 22:05:57 +0800

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,36 @@
+# time: O(1), space: O(n)
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = [] # min value at that index
+        # record "previous min"
+        # minStack[i] = minimum of stack[0:i+1]
+
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+        # if minStack empty, min(value, value)
+        # value = min(value, self.minStack[-1] if self.minStack else value)
+        if self.minStack: 
+            value = min(value, self.minStack[-1])
+        self.minStack.append(value)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        # no edge case, pop() is called on non-empty stack
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
